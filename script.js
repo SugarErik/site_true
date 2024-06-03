a37bd8b4c9da5b16c8c4146f435c9a96dd8c5308
Sweet-Cheese-Hackaton <>
2024-06-03 16:35:41 +0300

    zakaz

--- script.js
+++ script.js
@@ -4,17 +4,23 @@ function showMenu(category) {
         menuContent.innerHTML = `
             <h2>Еда</h2>
             <ul>
-                <li>Пицца - $10</li>
-                <li>Бургер - $8</li>
+                <li>Пицца - $10 <button onclick="order('Пицца', 10)">Заказать</button></li>
+                <li>Бургер - $8 <button onclick="order('Бургер', 8)">Заказать</button></li>
             </ul>
         `;
     } else if (category === 'drinks') {
         menuContent.innerHTML = `
             <h2>Напитки</h2>
             <ul>
-                <li>Кола - $2</li>
-                <li>Сок - $3</li>
+                <li>Кола - $2 <button onclick="order('Кола', 2)">Заказать</button></li>
+                <li>Сок - $3 <button onclick="order('Сок', 3)">Заказать</button></li>
             </ul>
         `;
     }
+}
+
+function order(item, price) {
+    const orderDetails = `Вы заказали: ${item} за $${price}`;
+    alert(orderDetails);
+    Telegram.WebApp.sendData(orderDetails);  // Отправка данных в Telegram WebApp
 }
